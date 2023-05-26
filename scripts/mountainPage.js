b02b7f9f882f1a54f6a12580b0d66a52dc8a1180
Zhayida Haishan <130613310+ZayidaQ@users.noreply.github.com>
2023-05-25 23:38:48 -0700

    update

--- scripts/mountainPage.js
+++ scripts/mountainPage.js
@@ -8,46 +8,58 @@ import { createNewDropdown } from "./script.js";
 const mountainsList = document.querySelector("#mountainsList");
 const displayMountainInfo = document.querySelector("#displayMountainInfo");
 
-window.addEventListener("load", () => {
+// On page load
+window.onload = () => {
   createNewDropdown(getMountainNames(mountainsArray), "#mountainsList");
   mountainsList.addEventListener("change", createMountainInfoCard);
-});
+};
 
-function getMountainNames(myArrayList) {
-  return myArrayList.map(obj => obj.name).sort();
+// Get mountain names from the array
+function getMountainNames(array) {
+  return array.map((obj) => obj.name).sort();
 }
 
+// Create info card for the selected mountain and display
 function createMountainInfoCard() {
-  const index = mountainsList.selectedIndex;
-  const selectedText = mountainsList[index].text;
-  displayMountainInfo.innerHTML = "";
+  const selectedMountain = mountainsList.value;
+  const mountain = mountainsArray.find((obj) => obj.name === selectedMountain);
 
-  mountainsArray.filter(obj => obj.name === selectedText).forEach(obj => {
-    const card = document.createElement("div");
-    card.classList.add("card", "mb-3");
-    card.innerHTML = `
-      <img src="images/${obj.img}" class="card-img-top img-fluid" alt="...">
-      <h5 class="card-header">${obj.name}</h5>
-      <div class="card-body">
-        <p class="card-text">${obj.desc}</p>
-        <p class="card-text">Elevation: ${obj.elevation} feet</p>
-        <p class="card-text">Effort: ${obj.effort}</p>
+  displayMountainInfo.innerHTML = `
+  <div class="card mb-3 mountain-card">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="images/${mountain.img}" class="card-img-top img-fluid mountain-image" alt="...">
       </div>
-    `;
-    displayMountainInfo.appendChild(card);
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title mountain-title">${mountain.name}</h5>
+          <p class="card-text mountain-description">${mountain.desc}</p>
+          <p class="card-text mountain-info">Elevation: ${mountain.elevation} feet</p>
+          <p class="card-text mountain-info">Effort: ${mountain.effort}</p>
+          <p><i class="bi bi-sunrise"></i> Sunrise: <span id="sunriseTime"></span> UTC</p>
+          <p><i class="bi bi-sunset-fill"></i> Sunset: <span id="sunsetTime"></span> UTC</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
 
-    const cardBody = card.querySelector(".card-body");
-    getSunsetForMountain(obj.coords.lat, obj.coords.lng).then(data => {
-      cardBody.innerHTML += `<p><i class="bi bi-sunrise"></i> Sunrise: ${data.results.sunrise} UTC</p>`;
-      cardBody.innerHTML += `<p><i class="bi bi-sunset-fill"></i> Sunset: ${data.results.sunset} UTC</p>`;
+
+
+
+  const sunriseTime = document.querySelector("#sunriseTime");
+  const sunsetTime = document.querySelector("#sunsetTime");
+
+  getSunsetForMountain(mountain.coords.lat, mountain.coords.lng)
+    .then((data) => {
+      sunriseTime.textContent = data.results.sunrise;
+      sunsetTime.textContent = data.results.sunset;
     });
-  });
 }
 
+// Function to fetch the sunrise/sunset times
 async function getSunsetForMountain(lat, lng) {
-  const response = await fetch(
-    `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
-  );
+  const response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
   const data = await response.json();
   return data;
 }
+
--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ import { locationsArray } from "./locationData.js";
 import { parkTypesArray } from "./parkTypeData.js";
 import { nationalParksArray } from "./nationalParkData.js";
 
-const filterSearchBy = ["Location", "Park Type"];
 const filterSearchByList = document.querySelector("#filterSearchBy");
 const labelSearchTypeList = document.querySelector("#labelSearchTypeList");
 const searchTypeList = document.querySelector("#searchTypeList");
@@ -14,15 +13,15 @@ let filterSelect = "";
 
 window.addEventListener("load", () => {
   nationalParksArray.forEach(obj => {
-    Object.keys(obj).forEach(k => {
-      if (obj[k] === 0) {
-        obj[k] = "";
+    for (const key in obj) {
+      if (obj[key] === 0) {
+        obj[key] = "";
       }
-    });
+    }
   });
 
   btnViewAll.addEventListener("click", onBtnViewAll);
-  createNewDropdown(filterSearchBy, "#filterSearchBy");
+  createNewDropdown(filterSearchByList, ["Location", "Park Type"]);
   filterSearchByList.addEventListener("change", onChangeFilterSearchBy);
   searchTypeList.addEventListener("change", onChangeSearchTypeList);
   parksList.addEventListener("change", onChangeParksList);
@@ -30,28 +29,25 @@ window.addEventListener("load", () => {
   hideElement(divParksList);
 });
 
-export function createNewDropdown(myArrayList, nameOfDropdown) {
-  const newDropdown = document.querySelector(nameOfDropdown);
-  newDropdown.innerHTML = '<option>Select one</option>';
-  myArrayList.forEach(element => {
-    const theOption = new Option(element);
-    newDropdown.appendChild(theOption);
+function createNewDropdown(dropdown, options) {
+  dropdown.innerHTML = '<option>Select one</option>';
+  options.forEach(option => {
+    dropdown.innerHTML += `<option>${option}</option>`;
   });
 }
 
 function onBtnViewAll() {
   resetElement(filterSearchByList);
-  createNewDropdown(filterSearchBy, "#filterSearchBy");
+  createNewDropdown(filterSearchByList, ["Location", "Park Type"]);
   resetElement(displayParksInfo);
-  nationalParksArray.forEach(obj => createParkInfoCard(obj));
+  nationalParksArray.forEach(createParkInfoCard);
   hideElement(labelSearchTypeList);
   hideElement(searchTypeList);
   hideElement(divParksList);
 }
 
 function onChangeFilterSearchBy() {
-  const index = filterSearchByList.selectedIndex;
-  const selectedText = filterSearchByList[index].text;
+  const selectedText = filterSearchByList.value;
   resetElement(searchTypeList);
   resetElement(displayParksInfo);
   showElement(labelSearchTypeList);
@@ -60,41 +56,36 @@ function onChangeFilterSearchBy() {
 
   if (selectedText === "Location") {
     labelSearchTypeList.innerHTML = "Choose a state/territory:";
-    createNewDropdown(locationsArray, "#searchTypeList");
+    createNewDropdown(searchTypeList, locationsArray);
     filterSelect = "Location";
   } else if (selectedText === "Park Type") {
     labelSearchTypeList.innerHTML = "Choose a park type:";
-    createNewDropdown(parkTypesArray, "#searchTypeList");
+    createNewDropdown(searchTypeList, parkTypesArray);
     filterSelect = "Park Type";
-  } else if (selectedText === "Select one") {
+  } else {
     hideElement(labelSearchTypeList);
     hideElement(searchTypeList);
   }
 }
 
 function onChangeSearchTypeList() {
-  const index = searchTypeList.selectedIndex;
-  const selectedText = searchTypeList[index].text;
+  const selectedText = searchTypeList.value;
   const matching = [];
   resetElement(parksList);
   resetElement(displayParksInfo);
-  // showElement(divParksList);
+  showElement(divParksList);
 
-  nationalParksArray.filter(obj => {
-    if (filterSelect === "Location") {
-      if (obj.State === selectedText) {
-        matching.push(obj.LocationName);
-        createParkInfoCard(obj);
-      }
-    } else if (filterSelect === "Park Type") {
-      if (obj.LocationName.toLowerCase().includes(selectedText.toLowerCase())) {
-        matching.push(obj.LocationName);
-        createParkInfoCard(obj);
-      }
+  nationalParksArray.forEach(obj => {
+    if (
+      (filterSelect === "Location" && obj.State === selectedText) ||
+      (filterSelect === "Park Type" && obj.LocationName.toLowerCase().includes(selectedText.toLowerCase()))
+    ) {
+      matching.push(obj.LocationName);
+      createParkInfoCard(obj);
     }
   });
 
-  createNewDropdown(matching, "#parksList");
+  createNewDropdown(parksList, matching);
   if (selectedText === "Select one") {
     hideElement(divParksList);
   }
@@ -102,37 +93,42 @@ function onChangeSearchTypeList() {
 }
 
 function onChangeParksList() {
-  const index = parksList.selectedIndex;
-  const selectedText = parksList[index].text;
+  const selectedText = parksList.value;
   resetElement(displayParksInfo);
 
-  nationalParksArray.find(element => {
-    if (element.LocationName === selectedText) {
-      createParkInfoCard(element);
+  nationalParksArray.forEach(obj => {
+    if (obj.LocationName === selectedText) {
+      createParkInfoCard(obj);
     }
   });
 }
 
 function createParkInfoCard(object) {
   const parkInfo = `
-    <div class="col-4">
-      <h5 class="card-header">${object.LocationName}</h5>
-      <div class="card-body">
-        <p><strong>Location ID:</strong> ${object.LocationID.toUpperCase()}</p>
-        <p><strong>Address:</strong> ${object.Address}, ${object.City}, ${object.State} ${object.ZipCode}</p>
-        <p><strong>Contact:</strong> ${object.Phone} ${object.Fax}</p>
+    <div class="row">
+      <div class="col">
+        <div class="park-info animated fadeIn">
+          <h5 class="park-info-title">${object.LocationName}</h5>
+          <div class="park-info-details">
+            <p><strong>Location ID:</strong> ${object.LocationID.toUpperCase()}</p>
+            <p><strong>Address:</strong> ${object.Address}, ${object.City}, ${object.State} ${object.ZipCode}</p>
+            <p><strong>Contact:</strong> ${object.Phone} ${object.Fax}</p>
+          </div>
+        </div>
       </div>
     </div>`;
 
-  displayParksInfo.innerHTML += parkInfo;
+  displayParksInfo.insertAdjacentHTML("beforeend", parkInfo);
 }
 
 function hideElement(element) {
   element.style.display = "none";
 }
+
 function showElement(element) {
   element.style.display = "flex";
 }
+
 function resetElement(element) {
   element.innerHTML = "";
 }
